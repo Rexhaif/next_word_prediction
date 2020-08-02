09660215c2f4034835210756998b5b2cb6581ce5
Daniil Larionov <rexhaif.io@gmail.com>
2020-08-02 22:13:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import torch
 import string
 
 from transformers import AlbertTokenizer, AlbertForMaskedLM
-bert_tokenizer = AlbertTokenizer.from_pretrained('bert-base-uncased')
-bert_model = AlbertForMaskedLM.from_pretrained('bert-base-uncased').eval()
+albert_tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
+albert_model = AlbertForMaskedLM.from_pretrained('albert-base-v2').eval()
 
 top_k = 10
 
@@ -33,9 +33,9 @@ def encode(tokenizer, text_sentence, add_special_tokens=True):
 def get_all_predictions(text_sentence, top_clean=5):
     # ========================= BERT =================================
     print(text_sentence)
-    input_ids, mask_idx = encode(bert_tokenizer, text_sentence)
+    input_ids, mask_idx = encode(albert_tokenizer, text_sentence)
     with torch.no_grad():
-        predict = bert_model(input_ids)[0]
-    bert = decode(bert_tokenizer, predict[0, mask_idx, :].topk(top_k).indices.tolist(), top_clean)
+        predict = albert_model(input_ids)[0]
+    albert = decode(albert_tokenizer, predict[0, mask_idx, :].topk(top_k).indices.tolist(), top_clean)
 
-    return {'bert': bert}
+    return {'albert': albert}
